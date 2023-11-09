7ea54e9264b0fd4863027cf278a9800b0f1aad9f
rizk <rizkdotai@gmail.com>
2023-11-09 14:29:33 +0200

    Add HTML skeleton

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+module: {
+rules: [
+{
+test: /\.css$/,use:['style-loader',{loader:'css-loader', options: {importLoaders: 1}}, 'postcss-loader']
+}
+]
+},
+devServer: { watchContentBase: true, contentBase:path.resolve(__dirname, 'dist'), open: true
+}
+};
