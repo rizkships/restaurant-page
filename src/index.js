b89b9a9f0272a8b351d3bce5a57bc7cb6e90a6d1
rizk <rizkdotai@gmail.com>
2023-11-10 18:40:20 +0200

    Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import "./styles.css"
 
+import { createHeader } from './header.js';
+import { createHome } from './home.js';
+import { createMenu } from './menu.js';
+import { createContact } from './contact.js';
+
+const contentContainer = document.getElementById('content');
+
+function loadContent(module) {
+    // Clear existing content
+    contentContainer.innerHTML = '';
+    // Append new content
+    contentContainer.appendChild(module());
+}
+
+document.getElementById('homeTab').addEventListener('click
