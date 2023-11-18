7e4a2143c1af601b2b1ef0029b9c762d191bb78c
rizk <rizkdotai@gmail.com>
2023-11-18 16:48:35 +0200

    Create menu page

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,47 @@
 // menu.js
 
+const createMenuPage = () => {
+const content = document.querySelector('#content');
+const pageContent = document.createElement('div');
+pageContent.classList.add('page-content');
+
+
+const heading = document.createElement('h1');
+heading.textContent = 'Our Menu';
+
+const menuList = document.createElement('ul');
+const menuItem1 = document.createElement('li');
+menuItem1.textContent = 'Steak';
+menuList.appendChild(menuItem1)
+
+pageContent.appendChild(heading);
+pageContent.appendChild(menuList);
+content.appendChild(pageContent);
+
+}
+
+export default createMenuPage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //import './styles.css';
 
 
 
 
 // menu.js
-
+/*
 export function loadMenuPage() {
     const contentDiv = document.getElementById('content');
   
